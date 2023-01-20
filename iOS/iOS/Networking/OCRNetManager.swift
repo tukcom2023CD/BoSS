cf159ebc6abba7dc102aca565c317a980344f184
Jeong Dong <eejxdong99@gmail.com>
2023-01-20 17:26:54 +0900

    feat: OCR API 호출 코드 수정 및 모델 추가

--- iOS/iOS/Networking/OCRNetManager.swift
+++ iOS/iOS/Networking/OCRNetManager.swift
@@ -8,45 +8,51 @@
 import Foundation
 import Alamofire
 
-
-// Naver Clova OCR
+// Naver Clova OCR API 호출을 위한 싱글톤 패턴 클래스
 class OCRNetManager {
     static let shared = OCRNetManager()
     private init() {}
     
     // API 호출 코드
-    // - parameter image: Receipt image
-    // - return: Receipt Data
-//    func requestReceiptData() {
-//        let url = Bundle.main.OCR_API_URL
-//        let secretKey = Bundle.main.OCR_API_KEY
-//
-//        let headers: HTTPHeaders = [
-//            "Content-Type" : "multipart/form-data",
-//            "X-OCR-SECRET" : secretKey
-//        ]
-//
-//        // let file = 전달받은 이미지.pngData()
-//
-//        let message = "{\"version\": \"V2\",\"requestId\": \"demo\",\"timestamp\": 1584062336793,\"images\": [{ \"format\": \"png\", \"name\": \"demo\" }]}"
-//
-//        AF.upload(multipartFormData: { (multipartFormData) in
-//
-//            multipartFormData.append(file!, withName: "file", fileName: "test.png", mimeType: "multipart/form-data")
-//
-//            multipartFormData.append(message.data(using: .utf8)!, withName: "message")
-//
-//
-//        }, to: url, method: .post, headers: headers).responseJSON { (response) in
-//
-//            guard let statusCode = response.response?.statusCode else { return }
-//            print(statusCode)
-//            print(response.data)
-//
-//            guard let safeData = response.data else { return }
-//
-//            let decodedData = try? JSONDecoder().decode(OCRResult.self, from: safeData)
-//            dump(decodedData?.images[0].receipt.result)
-//        }
-//    }
+    /// - parameter image: Receipt image
+    /// - parameter completion: ReceiptData를 이용한 작업
+    func requestReceiptData(image: UIImage, completion: @escaping (ReceiptData)->()) {
+        
+        let url = Bundle.main.OCR_API_URL
+        let secretKey = Bundle.main.OCR_API_KEY
+
+        let headers: HTTPHeaders = [
+            "Content-Type" : "multipart/form-data",
+            "X-OCR-SECRET" : secretKey
+        ]
+
+        let file = image.pngData()!
+        let requestId = UUID().uuidString
+        let timeStamp = Date().timeIntervalSinceReferenceDate / 10
+        let message = "{\"version\": \"V2\",\"requestId\": \"\(requestId)\",\"timestamp\": \(timeStamp),\"images\": [{ \"format\": \"png\", \"name\": \"demo\" }]}"
+        
+        AF.upload(multipartFormData: { (multipartFormData) in
+
+            multipartFormData.append(file, withName: "file", fileName: "test.png", mimeType: "multipart/form-data")
+
+            multipartFormData.append(message.data(using: .utf8)!, withName: "message")
+
+
+        }, to: url, method: .post, headers: headers).responseJSON { (response) in
+
+            guard let statusCode = response.response?.statusCode else { return }
+            print(statusCode)
+
+            guard let safeData = response.data else { return }
+
+            let decodedData = try? JSONDecoder().decode(OCRResult.self, from: safeData)
+            guard let ocrResult = decodedData else { return }
+            
+            let receiptData = ocrResult.images[0].receipt.receiptData
+            dump(receiptData)
+            
+            completion(receiptData)
+            
+        }
+    }
 }
