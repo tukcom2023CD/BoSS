6e913e70243387fb2991e618809d5c0f69e2bd14
hyuuuun <pam4522@gmail.com>
2023-01-24 22:27:54 +0900

    feat: date 표시

--- iOS/iOS/Home/CalendarTableViewCell.swift
+++ iOS/iOS/Home/CalendarTableViewCell.swift
@@ -18,6 +18,9 @@ class CalendarTableViewCell: UITableViewCell,FSCalendarDelegate {
 //        calendarImg.layer.cornerRadius = 5//숫자 클수록 둥글게
 //        calendarImg.layer.masksToBounds = true
 //        calendarImg.clipsToBounds = true
+//        calendar.appearance.titleFont = UIFont.systemFont(ofSize: 30)   // 달력 글씨 크기
+        
+        calendar.delegate = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -25,5 +28,10 @@ class CalendarTableViewCell: UITableViewCell,FSCalendarDelegate {
 
         // Configure the view for the selected state
     }
-    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-dd EEEE"
+        let string = formatter.string(from: date)
+        print("\(string)")
+    }
 }
