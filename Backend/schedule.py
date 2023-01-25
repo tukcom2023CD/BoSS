ce4f28d2553795ff1a83026a8b945a481eb48246
junhxx <pssoun1213@naver.com>
2023-01-25 14:41:23 +0900

    feat : 여행 일정 관련 api U, D 구현

--- Backend/schedule.py
+++ Backend/schedule.py
@@ -1,46 +1,66 @@
 import json
 from flask import Flask, request
-from flask_restx import Api, Resource, Api  # Api 구현을 위한 Api 객체 import
+from flask_restx import Api, Resource
 import pymysql
-import pandas as pd
+import connect
 
 # @app.route("/")
 
-# flask 객체 인스턴스 생성
-app = Flask(__name__)
+# flask 객체를 생섭합니다.
+app= Flask(__name__)
 
-# Api 객체 생성
-api = Api(app)
+# Api 객체를 생성합니다. 
+api= Api(app)
 
-# 신규 일정 생성 (C)
+# 일정 생성 (C)
 @api.route('/api/create/schedule')
-class CreateScheduleRecord(Resource):
+class CreateSchedule(Resource):
     def post(self):
-        title = (request.json.get('title')) # json 데이터에서 title 값 저장
-        region = (request.json.get('region')) # json 데이터에서 region 값 저장
-        start = (request.json.get('start')) # json 데이터에서 start 값 저장
-        stop = (request.json.get('stop')) # json 데이터에서 stop 값 저장
-        uid = (request.json.get('uid')) # json 데이터에서 uid 값 저장
-        conn = pymysql.connect(host='localhost', user='root', password='password', db='Boss', charset='utf8', autocommit=True) # 연결 설정
-        curs = conn.cursor() # cursor 객체설정
-        sql = f"insert into schedule(title, region, start, stop, uid) values('{title}', '{region}', '{start}', '{stop}', {uid})" # sql문 
-        curs.execute(sql) # sql문 수행
-        curs.close # cursor 닫기
-        conn.close # DB연결 닫기
-
-# 일정 데이터 가져오기 (R)
+        title= (request.json.get('title')) # json 데이터에서 title 값을 저장합니다.
+        region= (request.json.get('region')) # json 데이터에서 region 값을 저장합니다.
+        start= (request.json.get('start')) # json 데이터에서 start 값을 저장합니다.
+        stop= (request.json.get('stop')) # json 데이터에서 stop 값을 저장합니다.
+        uid= (request.json.get('uid')) # json 데이터에서 uid 값을 저장합니다.
+        
+        sql= f"insert into schedule(title, region, start, stop, uid) values('{title}', '{region}', '{start}', '{stop}', {uid})" # sql문을 생성합니다.
+        conn = connect.ConnectDB(sql) # DB와 연결합니다.
+        conn.execute() # sql문 수행합니다.
+        del conn # DB와 연결을 해제합니다.
+        
+# 일정 가져오기 (R)
 @api.route('/api/read/schedules/<int:uid>')  
-class ReadScheduleRecords(Resource):
+class ReadSchedule(Resource):
     def get(self, uid):
-      conn = pymysql.connect(host='localhost', user='root', password='password', db='Boss', charset='utf8', autocommit=True) # 연결 설정
-      curs = conn.cursor(pymysql.cursors.DictCursor) # cursor 객체설정 (결과값 딕셔너리 형태로 반환)
-      sql = f"select * from schedule where uid = {uid}"
-      curs.execute(sql) # 쿼리 실행
-      data = curs.fetchall() # 1개 결과값만 반환
-      data = json.dumps(data, ensure_ascii=False) # 딕셔너리 데이터 json 형식으로 변환
-      curs.close # cursor 닫기
-      conn.close # DB연결 닫기
-      return data # json 형식의 데이터 반환
+        sql = f"select * from schedule where uid = {uid}" # sql문을 생성합니다.
+        conn = connect.ConnectDB(sql) # DB와 연결합니다.
+        conn.execute() # sql문 수행합니다.
+        data = conn.fetch() # json 형식의 데이터를 가져옵니다.
+        del conn # DB와 연결을 해제합니다.
+        return data # josn 형식의 데이터를 반환합니다.
+      
+# 일정 업데이트 (U)
+@api.route('/api/update/schedule')
+class UpdateSchedule(Resource):
+    def post(self):
+        sid = (request.json.get('sid')) # json 데이터에서 sid 값을 저장합니다.
+        title = (request.json.get('title')) # json 데이터에서 title 값을 저장합니다.
+        region = (request.json.get('region')) # json 데이터에서 region 값을 저장합니다.
+        start = (request.json.get('start')) # json 데이터에서 start 값을 저장합니다.
+        stop = (request.json.get('stop')) # json 데이터에서 stop 값을 저장합니다.
+        
+        sql = f"update schedule set title ='{title}', region ='{region}', start ='{start}', stop ='{stop}' where sid = {sid}" # sql문을 생성합니다.
+        conn = connect.ConnectDB(sql) # DB와 연결합니다.
+        conn.execute() # sql문 수행합니다.
+        del conn # DB와 연결을 해제합니다.
 
+# 일정 삭제 (D)
+@api.route('/api/delete/schedule/<int:sid>')  
+class DeleteSchedule(Resource):
+    def get(self, sid):
+        sql = f"delete from schedule where sid = {sid}" # sql문을 생성합니다.
+        conn = connect.ConnectDB(sql) # DB와 연결합니다.
+        conn.execute() # sql문 수행합니다.
+        del conn # DB와 연결을 해제합니다.
+       
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True) 
