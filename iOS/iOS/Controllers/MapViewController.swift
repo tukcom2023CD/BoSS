4449fa1665b47a67bb70cc544d47fe3d738116b8
Jeong Dong <eejxdong99@gmail.com>
2023-01-28 04:36:36 +0900

    feat: 기간 설정 버튼 추가

--- iOS/iOS/Controllers/MapViewController.swift
+++ iOS/iOS/Controllers/MapViewController.swift
@@ -8,17 +8,21 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import CalendarDateRangePicker
 
 class MapViewController: UIViewController {
-
-    @IBOutlet weak var mapView: UIView!
     
+    @IBOutlet weak var calendarButton: UIButton!
+    
+    @IBOutlet weak var subView: UIView!
+    
+    var viewBlurEffect: UIVisualEffectView!
     var map: GMSMapView!
     var places: [Place]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         loadMapView()
         requestPlaceData()
     }
@@ -28,10 +32,12 @@ class MapViewController: UIViewController {
         
         // 카메라 위치
         let camera = GMSCameraPosition(latitude: 36, longitude: 127.5, zoom: 7)
-        map = GMSMapView(frame: CGRect(x: 0, y: 0, width: mapView.frame.size.width, height: mapView.frame.size.height), camera: camera)
-        mapView.addSubview(map)
+        map = GMSMapView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), camera: camera)
+        view.addSubview(map)
+        
+        view.bringSubviewToFront(calendarButton)
     }
-
+    
     // 여행지 데이터 호출 및 지도에 마커 표시
     func requestPlaceData() {
         PlaceNetManager.shared.read { places in
@@ -46,4 +52,36 @@ class MapViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func calendarButtonTapped(_ sender: UIButton) {
+
+        let dateRangePickerVC = CalendarDateRangePickerViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        dateRangePickerVC.delegate = self
+        let navigationController = UINavigationController(rootViewController: dateRangePickerVC)
+        
+        
+        present(navigationController, animated: true)
+        
+    }
+
+}
+
+extension MapViewController: CalendarDateRangePickerViewControllerDelegate {
+    func didCancelPickingDateRange() {
+        
+    }
+    
+    func didPickDateRange(startDate: Date!, endDate: Date!) {
+        
+    }
+    
+    func didSelectStartDate(startDate: Date!) {
+        
+    }
+    
+    func didSelectEndDate(endDate: Date!) {
+        
+    }
+    
+    
 }
