6ab6bc6d9fe5f4701147b14c024bea437dea86be
Jeong Dong <eejxdong99@gmail.com>
2023-01-31 13:38:05 +0900

    refactor: Bundle+Ext 파일 리펙토링

--- iOS/iOS/Extensions/Bundle+Ext.swift
+++ iOS/iOS/Extensions/Bundle+Ext.swift
@@ -8,42 +8,35 @@
 import Foundation
 
 extension Bundle {
-    // 생성한 .plist 파일 경로 불러오기
+    
     var GOOGLE_API_KEY: String {
-        guard let file = self.path(forResource: "SecretKey", ofType: "plist") else { return "" }
-        
-        // .plist를 딕셔너리로 받아오기
-        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
-        
-        // 딕셔너리에서 값 찾기
-        guard let key = resource["GOOGLE_API_KEY"] as? String else {
-            fatalError("GOOGLE_API_KEY error")
-        }
-        return key
+        return getSecretKey(key: "GOOGLE_API_KEY")
     }
     
     var OCR_API_URL: String {
-        guard let file = self.path(forResource: "SecretKey", ofType: "plist") else { return "" }
-        
-        // .plist를 딕셔너리로 받아오기
-        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
-        
-        // 딕셔너리에서 값 찾기
-        guard let key = resource["OCR_API_URL"] as? String else {
-            fatalError("OCR_API_URL error")
-        }
-        return key
+        return getSecretKey(key: "OCR_API_URL")
     }
     
     var OCR_API_KEY: String {
+        return getSecretKey(key: "OCR_API_KEY")
+    }
+    
+    var REST_API_URL: String {
+        return getSecretKey(key: "REST_API_URL")
+    }
+    
+    // SecretKey.plist에서 값 가져오기
+    /// - parameter key : SecretKey.plist에 등록된 Key
+    /// - returns : Key에 해당하는 Value
+    func getSecretKey(key: String) -> String {
         guard let file = self.path(forResource: "SecretKey", ofType: "plist") else { return "" }
         
         // .plist를 딕셔너리로 받아오기
         guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
         
         // 딕셔너리에서 값 찾기
-        guard let key = resource["OCR_API_KEY"] as? String else {
-            fatalError("OCR_API_KEY error")
+        guard let key = resource[key] as? String else {
+            fatalError("\(key) error")
         }
         return key
     }
---
+++ iOS/iOS/Networking/ScheduleNetManager.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleNetManager.swift
+//  iOS
+//
+//  Created by 이정동 on 2023/01/31.
+//
+
+import Foundation
+
+class ScheduleNetManager {
+    static let shared = ScheduleNetManager()
+    private init() {}
+    
+    func create(schedule: Schedule, completion: @escaping ()->()) {
+        
+    }
+}
