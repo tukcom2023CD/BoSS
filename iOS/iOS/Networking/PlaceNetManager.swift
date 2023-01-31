c7a148ef03aa52a57588b72252cef1390b6b46e0
Jeong Dong <eejxdong99@gmail.com>
2023-01-31 13:47:21 +0900

    feat: Schedule Create API 호출 코드 구현

--- iOS/iOS/Networking/PlaceNetManager.swift
+++ iOS/iOS/Networking/PlaceNetManager.swift
@@ -16,7 +16,7 @@ class PlaceNetManager {
     // 여행지 데이터 불러오기
     /// - parameter completion : Place 데이터를 이용한 화면 작업
     func read(completion: @escaping ([Place])->()) {
-        guard let url = URL(string: "http://127.0.0.1:5001/api/place/read/2") else { return }
+        guard let url = URL(string: "\(Bundle.main.REST_API_URL)/api/place/read/2") else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
--- iOS/iOS/Networking/ScheduleNetManager.swift
+++ iOS/iOS/Networking/ScheduleNetManager.swift
@@ -8,10 +8,45 @@
 import Foundation
 
 class ScheduleNetManager {
+    
     static let shared = ScheduleNetManager()
     private init() {}
     
     func create(schedule: Schedule, completion: @escaping ()->()) {
         
+        guard let url = URL(string: "\(Bundle.main.REST_API_URL)/api/schedule/create") else { return }
+        
+        guard let jsonData = try? JSONEncoder().encode(schedule) else {
+            print("Error: Trying to convert model to JSON data")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody = jsonData
+        
+        URLSession.shared.dataTask(with: request) { data, res, err in
+            
+            guard err == nil else {
+                print("Error: error calling POST")
+                print(err)
+                return
+            }
+            // 옵셔널 바인딩
+            guard let safeData = data else {
+                print("Error: Did not receive data")
+                return
+            }
+            // HTTP 200번대 정상코드인 경우만 다음 코드로 넘어감
+            guard let response = res as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
+                print("Error: HTTP request failed")
+                return
+            }
+            
+            // 원하는 모델이 있다면, JSONDecoder로 decode코드로 구현 ⭐️
+            print(String(decoding: safeData, as: UTF8.self))
+        }.resume()
     }
 }
