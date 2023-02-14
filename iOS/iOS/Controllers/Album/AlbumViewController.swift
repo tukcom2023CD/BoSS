284e20946c573014df24d21664bcea36fb0a5e79
junhxx <pssoun1213@naver.com>
2023-02-14 17:58:59 +0900

    chore : 서치바 수정

--- iOS/iOS/Controllers/Album/AlbumViewController.swift
+++ iOS/iOS/Controllers/Album/AlbumViewController.swift
@@ -8,26 +8,33 @@
 import UIKit
 
 class AlbumViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    
-    
-    @IBOutlet weak var searchBar: UISearchBar!
-    
-    
-    
+        
     // 예시 이미지 url
     let imageUrl : String = "https://placeimg.com/480/480/arch"
     // 간격 수치 설정
     let sectionInsets = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
     
+
+    let categoryArray = ["인간", "동물", "자연", "건축물"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // UISearchController 객체 생성
-        let searchController = UISearchController(searchResultsController: nil)
-        self.navigationItem.searchController = searchController
-        
+        setSearchBar()
     }
+    
+    // 서치바 생성 및 설정
+    func setSearchBar () {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "카테고리 검색"
+        searchController.searchBar.scopeButtonTitles = categoryArray
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        // self.navigationItem.title = "앨범" // 네비게이션 아이템 타이틀
+        self.navigationItem.searchController = searchController
+    }
+    
+    
+    
     
     // 셀 개수 설정
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
