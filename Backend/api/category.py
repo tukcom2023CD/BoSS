3d448bcb2a0c78a9e7966bfe5985fba1c1c5bd92
Jeong Dong <eejxdong99@gmail.com>
2023-03-01 16:00:33 +0900

    refactor: flask rest api 코드 모듈화

---
+++ Backend/api/category.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from flask_restx import Api, Resource, Namespace
+import connect
+
+
+Category = Namespace('Category')
+
+# 사진에 대한 카테고리 생성 (C)
+        
+# 카테고리 종류 가져오기 (R)
+@Category.route('/api/categorytype/read')  
+class ReadCategortType(Resource):
+    def get(self):
+        sql = f"select DISTINCT category_name from category" # sql문을 생성합니다.
+        conn = connect.ConnectDB(sql) # DB와 연결합니다.
+        conn.execute() # sql문 수행합니다.
+        data = conn.fetch() # json 형식의 데이터를 가져옵니다.
+        del conn # DB와 연결을 해제합니다.
+        return jsonify({"categoryTypes": data}) # josn 형식의 데이터를 반환합니다.
+    
+# 사진의 카테고리 가져오기 (R)
+@Category.route('/api/categories/read/<int:phid>')  
+class ReadCategories(Resource):
+    def get(self, phid):
+        sql = f"select * from category where phid = {phid}" # sql문을 생성합니다.
+        conn = connect.ConnectDB(sql) # DB와 연결합니다.
+        conn.execute() # sql문 수행합니다.
+        data = conn.fetch() # json 형식의 데이터를 가져옵니다.
+        del conn # DB와 연결을 해제합니다.
+        return jsonify({"categories": data}) # josn 형식의 데이터를 반환합니다.
+    
+# 사진의 카테고리 업데이트 (U)
+        
+# 사진의 카테고리 삭제하기 (D)
+@Category.route('/api/categories/delete/<int:phid>')  
+class DeleteCategories(Resource):
+    def get(self, phid):
+        sql = f"delete from category where phid = {phid}" # sql문을 생성합니다.
+        conn = connect.ConnectDB(sql) # DB와 연결합니다.
+        conn.execute() # sql문 수행합니다.
+        del conn # DB와 연결을 해제합니다.
