0022c6479324c9a1acdfaa48d3d5428ef3e14842
DESKTOP-022022U\pam45 <pam4522@naver.com>
2023-03-14 23:36:02 +0900

    chore : /backend/ai/yolo 모델 추가

--- Backend/AI/yolov5.py
+++ Backend/AI/yolov5.py
@@ -5,7 +5,8 @@ from glob import glob
 def model(path):
 
   # model = torch.hub.load('ultralytics/yolov5', 'yolov5s', trust_repo=True)
-  model = torch.hub.load('ultralytics/yolov5', 'yolov5s', trust_repo=True, force_reload=True)
+  #model = torch.hub.load('ultralytics/yolov5', 'yolov5s', trust_repo=True, force_reload=True)
+  model = torch.hub.load('ultralytics/yolov5', 'custom', path='./best.pt', force_reload=True)
 
   with open('./AI/tags_ko.json', encoding= "UTF8") as f:
     tags_ko = json.load(f)
