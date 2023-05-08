879df23fee4cd077c0409ec02938bc35ca30a356
junhxx <pssoun1213@naver.com>
2023-05-09 07:13:15 +0900

    feat : 마이페이지 일정 편집 지역 선택 화면 UI 제약조건 코드 추가 및 지역 데이터 변경

--- iOS/iOS/Controllers/MyPage/SelectRegionViewController.swift
+++ iOS/iOS/Controllers/MyPage/SelectRegionViewController.swift
@@ -6,28 +6,106 @@
 //
 
 import UIKit
+import SwiftSoup
 
 // 여행 지역 선택 화면
 class SelectRegionViewController : UIViewController {
     
     // Delegate 프로토콜 채택
     weak var delegate: MyDelegate?
+    
+    @IBOutlet weak var buttonStackView: UIStackView!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var applyButton: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
 
-    let regionArray = ["서울", "부산", "인천", "전주", "태안", "춘천", "강릉"] // 지역 이름 배열
+    var regionArray = [String]() // 지역 이름 배열
     var selectedRegion = "" // 선택된 지역 이름
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        localPasing()
     }
     
     // UI 설정
     func setUI() {
-        cancelButton.layer.cornerRadius = 20
-        applyButton.layer.cornerRadius = 20
+        
+        // 화면 사이즈 값 저장
+        let screenWidthSize = UIScreen.main.bounds.size.width
+        let screenHeightSize = UIScreen.main.bounds.size.height
+        
+        // 컬렉션 뷰 UI 코드 설정
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        // 제약 조건 설정
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: screenHeightSize * -0.12),
+        ])
+        
+        // 버튼 스택 뷰 UI 코드 설정
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        // 제약 조건 설정
+        NSLayoutConstraint.activate([
+            // 상단으로 부터 떨어진 거리 설정
+            buttonStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: screenHeightSize * -0.04),
+            // X축 중심에 맞춤
+            buttonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            // 사이즈 설정
+        ])
+        
+        // 취소 버튼 UI 코드 설정
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        // 제약 조건 설정
+        NSLayoutConstraint.activate([
+            cancelButton.widthAnchor.constraint(equalToConstant: screenWidthSize * 0.3),
+            cancelButton.heightAnchor.constraint(equalToConstant: screenWidthSize * 0.1)
+        ])
+        self.cancelButton.layer.cornerRadius = screenWidthSize * 0.03
+        
+        // 선택 버튼 UI 코드 설정
+        applyButton.translatesAutoresizingMaskIntoConstraints = false
+        // 제약 조건 설정
+        NSLayoutConstraint.activate([
+            applyButton.widthAnchor.constraint(equalToConstant: screenWidthSize * 0.3),
+            applyButton.heightAnchor.constraint(equalToConstant: screenWidthSize * 0.1)
+        ])
+        self.applyButton.layer.cornerRadius = screenWidthSize * 0.03
+    }
+    
+    // 지역 정보 가져오기
+    func localPasing(){
+        guard let url = URL(string: "https://ko.wikipedia.org/wiki/%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%EC%9D%B8%EA%B5%AC%EC%88%9C_%EB%8F%84%EC%8B%9C_%EB%AA%A9%EB%A1%9D#%EA%B0%99%EC%9D%B4_%EB%B3%B4%EA%B8%B0") else {
+            return
+        }
+        do {
+            let html = try String(contentsOf: url, encoding: .utf8)
+            let doc: Document = try SwiftSoup.parse(html)
+            
+            let table: Element = try doc.select("table.wikitable").first()!
+            let rows: Elements = try table.select("tr")
+            
+            let numRows = rows.count
+            
+            for (i, row) in rows.enumerated() {
+                if i == numRows - 1 { // 맨 마지막 행에서는 반복문을 종료
+                    break
+                }
+                let cols: Elements = try row.select("td:eq(1)")
+                for col in cols {
+                    let data = try col.text()
+                    regionArray.append(data)
+                    print(try col.text())
+                }
+            }
+        } catch Exception.Error(let type, let message) {
+            print("\(type) : \(message)")
+        } catch {
+            print("error")
+        }
+        collectionView.reloadData()
     }
     
     // 변경 실패 오류
@@ -61,7 +139,7 @@ class SelectRegionViewController : UIViewController {
 }
 
 extension SelectRegionViewController : UICollectionViewDataSource, UICollectionViewDelegate {
-    // 셀 수 설정
+    // 셀 개수
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return regionArray.count
     }
@@ -74,18 +152,34 @@ extension SelectRegionViewController : UICollectionViewDataSource, UICollectionV
             return UICollectionViewCell()
         }
         
+        // 화면 사이즈 값 저장
+        let screenWidthSize = UIScreen.main.bounds.size.width
+        let screenHeightSize = UIScreen.main.bounds.size.height
+        
+        // 제약 조건 코드 설정
+        cell.regionName.translatesAutoresizingMaskIntoConstraints = false
+        cell.regionCheckImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cell.regionName.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: screenWidthSize * 0.06),
+            cell.regionName.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
+            cell.regionCheckImage.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -screenWidthSize * 0.06),
+            cell.regionCheckImage.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
+            cell.regionCheckImage.widthAnchor.constraint(equalToConstant : screenWidthSize * 0.075),
+            cell.regionCheckImage.heightAnchor.constraint(equalToConstant : screenWidthSize * 0.075)
+        ])
+        
         // cell UI 설정
-        cell.layer.cornerRadius = 25
+        cell.layer.cornerRadius = screenWidthSize * 0.06
         cell.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         // cell 그림자 설정
         cell.layer.masksToBounds = false
         cell.layer.shadowColor = UIColor.black.cgColor
-        cell.layer.shadowOpacity = 0.3
+        cell.layer.shadowOpacity = 0.2
         cell.layer.shadowOffset = CGSize(width: 0, height: 0)
         cell.layer.shadowRadius = 5
         
         // cell 내용 설정
-        cell.regionImage.layer.cornerRadius = 40
         cell.regionName.text = self.regionArray[indexPath.row]
         
         if self.selectedRegion == cell.regionName.text! {
@@ -96,7 +190,6 @@ extension SelectRegionViewController : UICollectionViewDataSource, UICollectionV
             cell.regionCheckImage.tintColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
             cell.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
-            
         return cell
     }
     
@@ -109,7 +202,6 @@ extension SelectRegionViewController : UICollectionViewDataSource, UICollectionV
             if self.selectedRegion == cell.regionName.text! {
                 self.selectedRegion = "" // 선택된 지역 이름 초기화
                 self.collectionView.reloadData()
-                
             }
             else {
                 self.selectedRegion = cell.regionName.text! // 선택된 지역 이름으로 설정
@@ -120,10 +212,6 @@ extension SelectRegionViewController : UICollectionViewDataSource, UICollectionV
 }
 
 class regionCell : UICollectionViewCell {
-    
-    
-    @IBOutlet weak var regionImage: UIImageView!
     @IBOutlet weak var regionName: UILabel!
     @IBOutlet weak var regionCheckImage: UIImageView!
-    
 }
