731cb090d64dc4266c4bbd601322770e5e065cdd
junhxx <pssoun1213@naver.com>
2023-05-18 17:18:04 +0900

    feat : 앨범 화면에서 카테고리 태그를 통해서 사진을 불러오는 기능 구현

--- iOS/iOS/Controllers/Album/AlbumViewController.swift
+++ iOS/iOS/Controllers/Album/AlbumViewController.swift
@@ -9,46 +9,143 @@ import UIKit
 
 class AlbumViewController: UIViewController {
 
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
     @IBOutlet weak var collectionView: UICollectionView!
     
-    // 간격 수치 값 설정 변수
-    let sectionInsets = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+    // 카테고리 셀 간격 수치 값 설정 변수
+    let categorySectionInsets = UIEdgeInsets(top: 1, left: 0, bottom: 10, right: 10)
+    
+    // 사진 셀 간격 수치 값 설정 변수
+    let ImageSectionInsets = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
    
-    var totalCategoryArray : [String] = [] // 전체 카테고리 배열
-    var selectedCategoryArray : [String] = ["사람", "동물", "기타"] // 선택된 카테고리 배열
+    var totalCategoryArray : [String] = ["사람", "동물", "식물", "건물", "바다", "산", "자동차", "기타"] // 전체 카테고리 배열
+    var selectedCategoryArray : [String] = [] // 선택된 카테고리 배열
+    var categoryButtonArray : [UIButton] = [] // 카테고리 버튼 배열
     var imageUrlArray : [String] = [] // 이미지 url 배열
     var imageCount : Int = 0  // 표시할 이미지 개수
     var currentImageUrl = "" // 선택된 이미지의 url
-
+    
+    // 화면 너비, 높이 값
+    var screenWidth = UIScreen.main.bounds.width
+    var screenHeight = UIScreen.main.bounds.height
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 태그 설정
+        categoryCollectionView.tag = 1
+        collectionView.tag = 2
+        
         setUI() // UI 설정
+        setCategoryButtonArray() // 카테고리 버튼 설정
         
         self.navigationItem.title = "앨범" // 네비게이션 아이템 타이틀
         
-        loadImagesWithCategory() // 이미지 불러오는 함수 호출
+        selectedCategoryArray = totalCategoryArray
         
+        loadImagesWithCategory() // 이미지 불러오는 함수 호출
+    
         // 앨범 사진에대한 삭제 버튼을 클릭한 경우 삭제후 새로고침 함수 호출
         NotificationCenter.default.addObserver(self, selector: #selector(reloadAfterDeleteImage), name: NSNotification.Name("ImageDeleteButtonPressed"), object: nil)
     }
     
     // UI 설정
     func setUI() {
+        
+        self.categoryCollectionView.translatesAutoresizingMaskIntoConstraints = false
         self.collectionView.translatesAutoresizingMaskIntoConstraints = false
         
+        // 카테고리 컬렉션 뷰
         NSLayoutConstraint.activate([
             // 위치 설정
-            self.collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            self.categoryCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            self.categoryCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            self.categoryCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            // 높이 설정
+            self.categoryCollectionView.heightAnchor.constraint(equalToConstant: screenHeight * 0.25)
+        ])
+        
+        // 이미지 컬렉션 뷰
+        NSLayoutConstraint.activate([
+            // 위치 설정
+            self.collectionView.topAnchor.constraint(equalTo: categoryCollectionView.bottomAnchor, constant: 0),
             self.collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
             self.collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             self.collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
         ])
     }
     
+    // 카테고리 버튼 배열 설정 함수
+    func setCategoryButtonArray() {
+        for category in totalCategoryArray {
+            
+            // 카테고리 버튼 생성
+            let categoryButton = UIButton(type: .custom)
+            
+            // 버튼 타이틀 설정
+            categoryButton.setTitle("\(category)", for: .normal)
+            
+            // 버튼 타이틀 폰트 사이즈
+            categoryButton.titleLabel!.font = UIFont.boldSystemFont(ofSize: 17)
+            
+            // 버튼 타이틀 컬러 설정
+            categoryButton.setTitleColor(.white, for: .normal)
+            
+            // 버튼 색상 설정
+            categoryButton.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+            
+            // 버튼 이미지 설정
+            let image = UIImage(systemName : "checkmark.circle.fill")
+            categoryButton.setImage(image, for: .normal)
+            
+            // 버튼 이미지 여백 공간 설정
+            categoryButton.imageEdgeInsets =  UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
+            
+            // 여백 공간 설정
+            categoryButton.contentEdgeInsets = UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 5)
+            
+            // 내용 맞게 사이즈 조절
+            categoryButton.sizeToFit()
+            
+            // 잘림 설정
+            categoryButton.clipsToBounds = true
+            
+            // 모서리 설정
+            categoryButton.layer.cornerRadius = 10
+            
+            // 눌렸을 때 동작
+            categoryButton.addAction(UIAction(handler: { _ in
+                // 선택된 카테고리 배열에 존재 하지 않는 다면
+                if !self.selectedCategoryArray.contains(categoryButton.titleLabel!.text!) {
+                    
+                    // 배열에 추가
+                    self.selectedCategoryArray.append(categoryButton.titleLabel!.text!)
+                    
+                    // 색상 및 이미지 변경
+                    categoryButton.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+                }
+                // 선택된 카테고리 배열에 존재 한다면
+                else {
+                    // 배열에서 삭제
+                    if let index = self.selectedCategoryArray.firstIndex(of: categoryButton.titleLabel!.text!) {
+                        self.selectedCategoryArray.remove(at: index)
+                    }
+                    
+                    // 색상 및 이미지 변경
+                    categoryButton.backgroundColor = #colorLiteral(red: 0.6617937088, green: 0.6617937088, blue: 0.6617937088, alpha: 1)
+                }
+                self.loadImagesWithCategory() // 이미지 불러오는 함수 호출
+            }), for: .touchUpInside)
+            
+            // 카테고리 버튼 배열에 추가
+            categoryButtonArray.append(categoryButton)
+        }
+    }
+    
     // 사진 불러오는 함수
     @objc func loadImagesWithCategory() {
+        self.imageUrlArray = []
+        self.imageCount = 0 // 이미지 수 초기화
         let user = UserDefaults.standard.getLoginUser()! // 유저 정보 불러오기
         
         // 어떠한 카테고리도 선택되어 있지 않은 경우
@@ -64,12 +161,11 @@ class AlbumViewController: UIViewController {
             let group = DispatchGroup() // 비동기 함수 그룹
             for category in self.selectedCategoryArray {
                 group.enter() // 그룹에 추가
+                // self.imageUrlDict[category] = []
                 PhotoNetManager.shared.read(uid: user.uid!, category: category) { photos in
                     for photo in photos {
-                        if !(self.imageUrlArray.contains(photo.imageUrl)) {
-                            self.imageCount += 1
-                            self.imageUrlArray.append(photo.imageUrl)
-                        }
+                        self.imageUrlArray.append(photo.imageUrl)
+                        self.imageCount += 1
                     }
                     group.leave() // 그룹 떠남
                 }
@@ -105,96 +201,179 @@ extension AlbumViewController : UICollectionViewDelegate, UICollectionViewDataSo
     
     // 셀 개수 설정
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.imageCount
+    
+        if collectionView.tag == 1 {
+            return self.totalCategoryArray.count
+        } else {
+            return self.imageCount
+        }
     }
     
     // 셀 내용 설정
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCollectionViewCell", for: indexPath) as?
-                AlbumCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        
-        cell.imageView.contentMode = .scaleToFill // cell에 이미지가 꽉차도록 표시
-        cell.url = imageUrlArray[indexPath.row] // cell에 표시될 사진의 url 설정
-        cell.imageName = extractValues(from : cell.url) // 이미지 이름 설정
 
-        // 받아온 url을 통해 캐시키 생성
-        let cacheKey = NSString(string: cell.url)
-        
-        // 만약 저장된 캐시데이터가 있다면 해당 데이터로 이미지 설정
-        if let cachedImage = AlbumImageCacheManager.shared.object(forKey: cacheKey) {
-            DispatchQueue.main.async {
-                cell.imageView.image = cachedImage
+        if collectionView.tag == 1 {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCollectionViewCell", for: indexPath) as?
+                    CategoryCollectionViewCell else {
+                return UICollectionViewCell()
             }
-        } else { // 만약 캐시데이터가 없다면
-            DispatchQueue.global().async { // 멀티쓰레드 사용
-                let url = URL(string: cell.url)
-                // url 통해서 이미지 데이터 다운로드
-                if let data = try? Data(contentsOf: url!) {
-                    // 이미지 설정
-                    DispatchQueue.main.async {
-                        cell.imageView.image = UIImage(data: data)
+            
+            // 셀 컨텐트 뷰에 버튼 추가
+            cell.addSubview(self.categoryButtonArray[indexPath.row])
+            
+            // 셀 제약 조건 설정
+            self.categoryButtonArray[indexPath.row].translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                // 셀 카테고리 버튼 제약조건
+                self.categoryButtonArray[indexPath.row].centerXAnchor.constraint(equalTo: cell.contentView.centerXAnchor),
+                self.categoryButtonArray[indexPath.row].centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor)
+            ])
+            
+            // 프레임값 적용
+            view.layoutIfNeeded()
+            
+            return cell
+            
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCollectionViewCell", for: indexPath) as?
+                    AlbumCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            
+            cell.imageView.contentMode = .scaleToFill // cell에 이미지가 꽉차도록 표시
+            cell.url = imageUrlArray[indexPath.row] // cell에 표시될 사진의 url 설정
+            cell.imageName = extractValues(from : cell.url) // 이미지 이름 설정
+
+            // 받아온 url을 통해 캐시키 생성
+            let cacheKey = NSString(string: cell.url)
+            
+            // 만약 저장된 캐시데이터가 있다면 해당 데이터로 이미지 설정
+            if let cachedImage = AlbumImageCacheManager.shared.object(forKey: cacheKey) {
+                DispatchQueue.main.async {
+                    cell.imageView.image = cachedImage
+                }
+            } else { // 만약 캐시데이터가 없다면
+                DispatchQueue.global().async { // 멀티쓰레드 사용
+                    let url = URL(string: cell.url)
+                    // url 통해서 이미지 데이터 다운로드
+                    if let data = try? Data(contentsOf: url!) {
+                        // 이미지 설정
+                        DispatchQueue.main.async {
+                            cell.imageView.image = UIImage(data: data)
+                        }
+                        // 키, 밸류 값으로 캐시값 저장
+                        AlbumImageCacheManager.shared.setObject(UIImage(data: data)!, forKey: cacheKey)
                     }
-                    // 키, 밸류 값으로 캐시값 저장
-                    AlbumImageCacheManager.shared.setObject(UIImage(data: data)!, forKey: cacheKey)
                 }
             }
+            return cell
         }
-        return cell
     }
     
     
     // 셀 크기 설정
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = UIScreen.main.bounds.width // 컬렉션 뷰 너비값
-        let itemsPerRow: CGFloat = 3 // 각 행의 아이템 수
-        let widthPadding = sectionInsets.left * (itemsPerRow + 1) // 너비의 총 여백 공간
-        let cellWidth = (width - widthPadding) / itemsPerRow // 셀당 너비
-        let cellHeight = cellWidth // 셀당 높이 = 셀당 너비
-        return CGSize(width: cellWidth, height: cellHeight) // 셀당 너비, 높이 설정
+    
+        if collectionView.tag == 1 {
+            let width = self.categoryButtonArray[indexPath.row].frame.size.width
+            let height = self.categoryButtonArray[indexPath.row].frame.size.height
+            return CGSize(width: width, height: height) // 셀당 너비, 높이 설정
+        }
+        else {
+            let width = UIScreen.main.bounds.width // 컬렉션 뷰 너비값
+            let itemsPerRow: CGFloat = 3 // 각 행의 아이템 수
+            let widthPadding = ImageSectionInsets.left * (itemsPerRow + 1) // 너비의 총 여백 공간
+            let cellWidth = (width - widthPadding) / itemsPerRow // 셀당 너비
+            let cellHeight = cellWidth // 셀당 높이 = 셀당 너비
+            return CGSize(width: cellWidth, height: cellHeight) // 셀당 너비, 높이 설정
+        }
+    }
+        
+    // section inset 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        if collectionView.tag == 1 {
+            return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        }
+        else {
+            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        }
     }
     
     // 셀당 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return sectionInsets.left
+        
+        if collectionView.tag == 1 {
+            return categorySectionInsets.right
+        }
+        else {
+            return ImageSectionInsets.left
+        }
+        
     }
     
     // 라인당 간격 설정
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return sectionInsets.left
+        
+        if collectionView.tag == 1 {
+            return categorySectionInsets.bottom
+        }
+        else {
+            return ImageSectionInsets.left
+        }
     }
     
     // 특정 셀(이미지)가 눌렸을 때 -> 이미지 자세히(원본) 보기
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("click index=\(indexPath.row)")
         
-        // 현재 셀 가져오기
-        guard let currentCell = collectionView.cellForItem(at: indexPath) as? AlbumCollectionViewCell else {
-            return
+        if collectionView.tag == 1 {
+            // 현재 셀 가져오기
+            guard let currentCell = collectionView.cellForItem(at: indexPath) as? CategoryCollectionViewCell else {
+                return
+            }
         }
-        
-        // 선택된 이미지 url 값 저장
-        self.currentImageUrl = currentCell.url
-        
-        // 자세히 보기 화면 전환
-        guard let popupVC = self.storyboard?.instantiateViewController(identifier: "popupVC") as? AlbumImagePopUpController else {return}
-        popupVC.modalPresentationStyle = .overFullScreen
-        popupVC.modalTransitionStyle = .crossDissolve
-        
-        // 이미지 넘겨주기
-        guard let currentCellImage = currentCell.imageView.image else {return}
-        popupVC.image = currentCellImage
-        popupVC.imageName = currentCell.imageName
-    
-        // 화면 전환
-        self.present(popupVC, animated: true, completion: nil)
+        else {
+            // 현재 셀 가져오기
+            guard let currentCell = collectionView.cellForItem(at: indexPath) as? AlbumCollectionViewCell else {
+                return
+            }
+            
+            // 선택된 이미지 url 값 저장
+            self.currentImageUrl = currentCell.url
+            
+            // 자세히 보기 화면 전환
+            guard let popupVC = self.storyboard?.instantiateViewController(identifier: "popupVC") as? AlbumImagePopUpController else {return}
+            popupVC.modalPresentationStyle = .overFullScreen
+            popupVC.modalTransitionStyle = .crossDissolve
+            
+            // 이미지 넘겨주기
+            guard let currentCellImage = currentCell.imageView.image else {return}
+            popupVC.image = currentCellImage
+            popupVC.imageName = currentCell.imageName
+            
+            // 화면 전환
+            self.present(popupVC, animated: true, completion: nil)
+        }
     }
 }
 
-// 컬렉션 뷰 셀 클래스
+// 카테고리 컬렉션 뷰 셀
+class CategoryCollectionViewCell : UICollectionViewCell {
+}
+
+// 앨범 컬렉션 뷰 셀
 class AlbumCollectionViewCell : UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     var url : String!
     var imageName : (String?, String?, String?, String?)
+
+    override func prepareForReuse() {
+        for someView in self.subviews {
+            if let imageView = someView as? UIImageView {
+                imageView.removeFromSuperview()
+            }
+        }
+    }
 }
